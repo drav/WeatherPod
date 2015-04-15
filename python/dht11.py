003515b5e8f60ee9b64b5a1e473517aa463d5167
Loan Laux <spam@loanlaux.fr>
2015-04-15 20:59:49 +0200

    Integrated DHT11 sensor library to the Python code

---
+++ python/dht11.py
@@ -0,0 +1,90 @@
+def bin2dec(string_num):
+    return str(int(string_num, 2))
+
+def getTemperatureHumidity(pin, value):
+
+    data = []
+
+    GPIO.setmode(GPIO.BCM)
+
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.HIGH)
+    time.sleep(0.025)
+    GPIO.output(pin, GPIO.LOW)
+    time.sleep(0.02)
+
+    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    for i in range(0, 500):
+        data.append(GPIO.input(pin))
+
+    bit_count = 0
+    tmp = 0
+    count = 0
+    humidityBit = ""
+    temperatureBit = ""
+    crc = ""
+
+    try:
+    	while data[count] == 1:
+    		tmp = 1
+    		count = count + 1
+
+    	for i in range(0, 32):
+    		bit_count = 0
+
+    		while data[count] == 0:
+    			tmp = 1
+    			count = count + 1
+
+    		while data[count] == 1:
+    			bit_count = bit_count + 1
+    			count = count + 1
+
+    		if bit_count > 3:
+    			if i>=0 and i<8:
+    				humidityBit = humidityBit + "1"
+    			if i>=16 and i<24:
+    				temperatureBit = temperatureBit + "1"
+    		else:
+    			if i>=0 and i<8:
+    				humidityBit = humidityBit + "0"
+    			if i>=16 and i<24:
+    				temperatureBit = temperatureBit + "0"
+
+    except:
+    	print("ERR_RANGE")
+    	exit(0)
+
+    try:
+    	for i in range(0, 8):
+    		bit_count = 0
+
+    		while data[count] == 0:
+    			tmp = 1
+    			count = count + 1
+
+    		while data[count] == 1:
+    			bit_count = bit_count + 1
+    			count = count + 1
+
+    		if bit_count > 3:
+    			crc = crc + "1"
+    		else:
+    			crc = crc + "0"
+    except:
+    	print("ERR_RANGE")
+    	exit(0)
+
+    humidity = bin2dec(humidityBit)
+    temperature = bin2dec(temperatureBit)
+
+    if int(humidity) + int(temperature) - int(bin2dec(crc)) == 0:
+    	if value == 0:
+            return temperature
+
+        else if value == 1:
+            return humidity
+        
+    else:
+    	print("ERR_CRC")
---
+++ python/sensors.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+# System libraries
+
+import RPi.GPIO as GPIO
+import time
+
+# Custom libraries
+
+import dht11
+
+print("WeatherPod v0.1")
