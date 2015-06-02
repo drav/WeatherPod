0fdbb9a48f0d35532109bdb41e7249e50abebc0e
Loan Laux <spam@loanlaux.fr>
2015-06-02 16:50:25 +0200

    Started to implement BMP180 support

---
+++ python/bmp180.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import Adafruit_BMP.BMP085 as BMP085
+
+# Arguments parsing
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", "--temperature", help = "returns temperature in Celcius", action = "store_true")
+parser.add_argument("-p", "--pressure", help = "returns atmospheric pressure in Pascals", action = "store_true")
+parser.add_argument("-a", "--altitude", help = "returns absolute altitude in meters", action = "store_true")
+args = parser.parse_args()
+
+BMP180 = BMP085.BMP085()
+
+if args.temperature:
+    print(BMP180.read_temperature())
+
+elif args.pressure:
+    print(BMP180.read_pressure())
+
+elif args.altitude:
+    print(BMP180.read_altitude())
