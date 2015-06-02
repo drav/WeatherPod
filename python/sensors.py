c3fa61a5defd4b1b0005652532480dd1bcba5f15
Loan Laux <spam@loanlaux.fr>
2015-06-02 19:34:05 +0200

    Fixed a typo in the import section

--- python/sensors.py
+++ python/sensors.py
@@ -34,7 +34,7 @@ if args.random:
 else:
     import RPi.GPIO as GPIO
     import dht11
-    import Adafruit_DHT.BMP085 as BMP085
+    import Adafruit_BMP.BMP085 as BMP085
 
     BMP180 = BMP085.BMP085()
 
