66eb201be6518c59532d58e56a556115ff320e6b
Loan Laux <spam@loanlaux.fr>
2015-06-02 19:05:02 +0000

    First partly working sensors data acquisition

--- python/sensors.py
+++ python/sensors.py
@@ -62,16 +62,18 @@ def sendData():
             temp = None
             hum = None
 
-        pres = BMP180.read_pressure()
-        alti = BMP180.read_altitude()
+        pres = int(BMP180.read_pressure())
+        alti = int(BMP180.read_altitude())
 
     # Output data to the console if verbose mode enabled
 
     if args.verbose:
-        print(u"Temperature: " + str(temp, "utf-8") + u"°C")
-        print(u"Humidity: " + str(hum, "utf-8") + u"%")
-        print(u"Pressure: " + str(pres, "utf-8") + u"hPa")
-        print(u"Altitude: " + str(alti, "utf-8") + u"m")
+	if temp:
+        	print(u"Temperature: " + str(temp) + u"°C")
+        	print(u"Humidity: " + str(hum) + u"%")
+        
+	print(u"Pressure: " + str(pres) + u"hPa")
+        print(u"Altitude: " + str(alti) + u"m")
 
     # Send data to the server using an HTTP GET request
 
